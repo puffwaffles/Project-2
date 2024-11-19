c7d9b4aee168644d1882930aeae7ed1f1d1ac1fd
puffwaffles <katelynjepoon@gmail.com>
2024-11-19 01:40:56 -0800

    Added existance function and node class

--- forward.py
+++ forward.py
@@ -9,16 +9,40 @@ def generateforwardval():
     num = round(np.random.rand() * 100, 1) * -1
     return num
 
+#Checks if a list already exists in the dictionary
+def alreadyexists(currlist, usedlists):
+    exists = False
+    tuplelist = (*currlist, )
+    if (currlist in usedlists):
+        exists = True
+    return exists
+    
+
+
 def forwardselect(numfeatures):
     #Generate random value for no features
     initial = generateforwardval()
     print()
     print(f"Using no features and \"random\" evaluation, I get an accuracy of {initial * -1}%")
-    #Use priority queue to order feature sets by highest percentage
-    pq = PriorityQueue()
+    '''#Use priority queue to order feature sets by highest percentage
+    pq = PriorityQueue()'''
+    #Stores feature lists that were already examined. 
+    usedfeaturelists = {} 
+    #Create list of features
+    fulllist = np.arange(1, numfeatures + 1, 1)
+    print("Beginning search.")
+    #Store feature list with best accuracy
+    bestlist = []
+    #Store best accuracy
+    bestacc = 0.0
+    #Check if accuracy decreases or we encountered the full list
+    stop = False
+    '''
+    while (stop == False):
+
     for i in range(numfeatures):
         num = generateforwardval()
         print(f"({num}, {i})")
         pq.put((num, i))
     while (pq.empty() == False):
-        print(f"{pq.get()[0] * -1}%")
+        print(f"{pq.get()[0] * -1}%")'''
--- node.py
+++ node.py
@@ -1,7 +1,10 @@
+# https://www.w3schools.com/python/ref_func_sorted.asp
+# https://www.geeksforgeeks.org/python-difference-two-lists/
 # Create node object
 class Node:
     # Initilaize node object with list of features and accuracy
-    def _init_(self, featurelist, accuracy):
+    def _init_(self, parent, featurelist, accuracy):
+        self.parent = parent
         self.featurelist = featurelist
         self.accuracy = accuracy
     # Returns the list of features
@@ -10,3 +13,6 @@ class Node:
     # Returns the accuracy
     def getacc(self):
         return self.accuracy
+    # Returns the parent node
+    def getparent(self):
+        return self.parent
